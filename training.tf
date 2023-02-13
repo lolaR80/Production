5dd757b79b546240941c4eb5af0aa0525c145dac
lollypop <lolaradeusi@gmail.com>
2023-02-12 20:44:11 -0500

    ec2s3

---
+++ training.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIATZWA5PQCZQZRGCU4"
+  secret_key = "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"
+}
+resource "aws_vpc" "text-VPC" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "text-VPC"
+  }
+}
+resource "aws_subnet" "text-subnet" {
+  vpc_id            = aws_vpc.text-VPC.id
+  cidr_block        = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "text-Subnet"
+  }
+}
+
+resource "aws_subnet" "text-subnet-private" {
+  vpc_id            = aws_vpc.text-VPC.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "text-Subnet-private"
+  }
+}
+
+resource "aws_internet_gateway" "text-igw" {
+  vpc_id = aws_vpc.text-VPC.id
+
+  tags = {
+    Name = "text-igw"
+  }
+}
+
+resource "aws_instance" "text" {
+  ami           = "ami-0aa7d40eeae50c9a9" # us-east-1a
+  instance_type = "t2.micro"
+  subnet_id = "subnet-0bcf0a66b74e07d5e"
+  tags = {
+    "Name" = "text"
+  }
+}
+
+resource "aws_s3_bucket" "text-s3-buc" {
+  bucket = "my-oa-text-bucket"
+
+  tags = {
+    Name        = "text-s3-buc"
+    Environment = "Dev"
+  }
+}
